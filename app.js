52aa8ea345226c24e6e69c8d175f0ae491750c64
MatthewJOwens <mjowens@mail.com>
2020-06-23 18:41:31 -0600

    Adding notes

---
+++ app.js
@@ -0,0 +1,12 @@
+// So, maybe the contribution calendar may not be part of the API after all.
+// This disqualifies my project.
+// Can still get something working, though.
+// Maybe Brittany could use an app that gets contribution graphs from multiple people at once.
+// Maybe compare users.
+// Maybe have average pushes per day graphs
+// Maybe show pie chart of languages
+
+//People who get the contribution calendar (I think via site scraping):
+// Embed: https://shellscape.org/2016/08/31/embedding-github-contribution-calendar
+// Array of numbers: https://github.com/KaDw/github-calendar-api
+// 
