23f06f444a8f12f38323c1e09a6bd62b5c1a03b0
MatthewJOwens <mjowens@mail.com>
2020-06-24 20:54:29 -0600

    more notes, increased possible scope

--- app.js
+++ app.js
@@ -9,4 +9,13 @@
 //People who get the contribution calendar (I think via site scraping):
 // Embed: https://shellscape.org/2016/08/31/embedding-github-contribution-calendar
 // Array of numbers: https://github.com/KaDw/github-calendar-api
-// 
+// SVG screenshot (no dates): https://gist.github.com/jcouyang/6336168ecbbf4fbdc46e
+
+// Custom calendar like Github's
+// https://github.com/julienr114/vue-calendar-heatmap
+// https://github.com/topics/calendar-heatmap
+// https://github.com/kevinsqi/react-calendar-heatmap
+
+// ref: https://mintbean.io/project/506f7411-bdb1-4d34-b925-260e0d90589b for "languages they code in"
+// he uses React and fetch to get from /users/:username/repos then gets res.json() and if exists and > 0, new Map() and then a forEach(repo), if language, map.set(language, map.get(language)+1) else map.set(language, 1)
+//He also uses setFetching(true) and (false) but I don't see a while(setFetching) anywhere, maybe this is a React thing
